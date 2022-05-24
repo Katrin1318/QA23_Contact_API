fa751c11b7ab3df3636d6234080f98e6bd455e57
Katrin1318 <94367818+Katrin1318@users.noreply.github.com>
2022-05-24 16:22:27 +0200

    API Tests

---
+++ src/main/java/dto/AuthRequestDto.java
@@ -0,0 +1,18 @@
+package dto;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@ToString
+
+public class AuthRequestDto {
+
+    String email;
+    String password;
+
+}
